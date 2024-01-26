8e75d9973cbd1d520b894a66a470e149db1cb65a
Florian Ehmann <kontakt@florianehmann.de>
2024-01-26 14:55:55 +0100

    CLI Tests for Commands Without Arguments

---
+++ tests/cli.rs
@@ -0,0 +1,57 @@
+use anyhow::Result;
+use assert_cmd::Command;
+
+fn build_command() -> Result<Command> {
+    let cmd = Command::cargo_bin::<String>(env!("CARGO_PKG_NAME").into())?;
+    Ok(cmd)
+}
+
+#[test]
+fn test_workspace() -> Result<()> {
+    let mut cmd = build_command()?;
+
+    cmd.arg("workspace");
+    cmd.assert().success();
+
+    Ok(())
+}
+
+#[test]
+fn test_workspace_deactivate() -> Result<()> {
+    let mut cmd = build_command()?;
+
+    cmd.arg("workspace").arg("deactivate");
+    cmd.assert().success();
+
+    Ok(())
+}
+
+#[test]
+fn test_project_ls() -> Result<()> {
+    let mut cmd = build_command()?;
+
+    cmd.arg("project").arg("ls");
+    cmd.assert().success();
+
+    Ok(())
+}
+
+#[test]
+fn test_criterion_ls() -> Result<()> {
+    let mut cmd = build_command()?;
+
+    cmd.arg("criterion").arg("ls");
+    cmd.assert().success();
+
+    Ok(())
+}
+
+#[test]
+fn test_display() -> Result<()> {
+    let mut cmd = build_command()?;
+
+    cmd.arg("display");
+    cmd.assert().success();
+
+    Ok(())
+}
