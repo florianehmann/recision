efff931623fafc277391a88b8a2db05e48473f1e
Florian Ehmann <kontakt@florianehmann.de>
2024-01-28 16:03:58 +0100

    Test Config Dir Determination

--- src/config.rs
+++ src/config.rs
@@ -4,34 +4,41 @@ use std::path::PathBuf;
 
 use anyhow::Result;
 use dirs::config_dir;
+use mockall::automock;
 
 #[derive(Debug)]
-pub struct ConfigDirError {}
+pub struct ConfigError {
+    message: String,
+}
 
-impl Display for ConfigDirError {
+impl Display for ConfigError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "can't locate config directory")
+        write!(f, "ConfigError: {}", self.message)
     }
 }
 
-impl Error for ConfigDirError {}
+impl Error for ConfigError {}
 
-#[allow(unused)]
-#[cfg(not(test))]
-fn get_config_dir() -> Option<PathBuf> {
-    return config_dir()
+#[automock]
+trait ConfigDirProvider {
+    fn get_config_dir(&self) -> Option<PathBuf>;
 }
 
-#[cfg(test)]
-fn get_config_dir() -> Option<PathBuf> {
-    return config_dir()
-}
+struct DefaultConfigDirProvider;
 
-fn get_config_file() -> Result<PathBuf> {
-    match get_config_dir() {
-        Some(path) => return Ok(path),
-        None => Err((ConfigDirError {}).into()),
+impl ConfigDirProvider for DefaultConfigDirProvider {
+    fn get_config_dir(&self) -> Option<PathBuf> {
+        return config_dir();
     }
+}
+
+fn get_config_dir(provider: &dyn ConfigDirProvider) -> Result<PathBuf> {
+    return provider.get_config_dir().ok_or(
+        (ConfigError {
+            message: "can't locate config directory".to_string(),
+        })
+        .into(),
+    );
 }
 
 #[cfg(test)]
@@ -39,7 +46,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_config_file() {
-        let _ = get_config_file();
+    fn test_get_real_config_dir() {
+        get_config_dir(&DefaultConfigDirProvider {}).unwrap();
+    }
+
+    #[test]
+    fn test_get_mock_config_dir() {
+        let mut provider = MockConfigDirProvider::new();
+        provider
+            .expect_get_config_dir()
+            .return_const(PathBuf::from("/tmp/config"));
+
+        get_config_dir(&provider).unwrap();
+    }
+
+    #[test]
+    fn test_fail_get_config_dir() {
+        let mut provider = MockConfigDirProvider::new();
+        provider.expect_get_config_dir().return_const(None);
+
+        let result = get_config_dir(&provider);
+
+        assert!(result.is_err());
     }
 }
